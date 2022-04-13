82fe4c9dcab4a88646d108cb2cfb7808c60c3caf
AnilShatharashi <anil.shatharasi@gmail.com>
2022-04-13 02:52:03 +0200

    Moved the ui things to presentation/ui package

--- app/src/main/java/com/anilsandroidtraining/catastrophic/presentation/ui/CatListFragment.kt
+++ app/src/main/java/com/anilsandroidtraining/catastrophic/presentation/ui/CatListFragment.kt
@@ -1,4 +1,4 @@
-package com.anilsandroidtraining.catastrophic
+package com.anilsandroidtraining.catastrophic.presentation.ui
 
 import android.os.Bundle
 import android.view.LayoutInflater
--- app/src/main/java/com/anilsandroidtraining/catastrophic/presentation/ui/MainActivity.kt
+++ app/src/main/java/com/anilsandroidtraining/catastrophic/presentation/ui/MainActivity.kt
@@ -1,15 +1,13 @@
-package com.anilsandroidtraining.catastrophic
+package com.anilsandroidtraining.catastrophic.presentation.ui
 
 import android.os.Bundle
-import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.navigateUp
 import androidx.navigation.ui.setupActionBarWithNavController
-import android.view.Menu
-import android.view.MenuItem
 import androidx.navigation.fragment.NavHostFragment
+import com.anilsandroidtraining.catastrophic.R
 import com.anilsandroidtraining.catastrophic.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
