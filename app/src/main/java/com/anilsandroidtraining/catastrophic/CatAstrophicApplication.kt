45ae6f1ff294fb25f5a644b96db2e32b72db09cd
AnilShatharashi <anil.shatharasi@gmail.com>
2022-04-13 03:12:49 +0200

    Introduced Hilt DependencyInjection Framework

---
+++ app/src/main/java/com/anilsandroidtraining/catastrophic/CatAstrophicApplication.kt
@@ -0,0 +1,7 @@
+package com.anilsandroidtraining.catastrophic
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class CatAstrophicApplication : Application()
--- app/src/main/java/com/anilsandroidtraining/catastrophic/presentation/ui/MainActivity.kt
+++ app/src/main/java/com/anilsandroidtraining/catastrophic/presentation/ui/MainActivity.kt
@@ -9,7 +9,9 @@ import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.fragment.NavHostFragment
 import com.anilsandroidtraining.catastrophic.R
 import com.anilsandroidtraining.catastrophic.databinding.ActivityMainBinding
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
 
     private lateinit var appBarConfiguration: AppBarConfiguration
