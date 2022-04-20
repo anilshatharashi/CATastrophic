889640837e5b9eb87fbb96ee8105eb679d149614
AnilShatharashi <anil.shatharasi@gmail.com>
2022-04-20 02:15:31 +0200

    Now displaying images in squareImageView

---
+++ app/src/main/java/com/zaloracasestudy/catastrophic/presentation/ui/customview/SquareImageView.kt
@@ -0,0 +1,16 @@
+package com.zaloracasestudy.catastrophic.presentation.ui.customview
+
+import android.content.Context
+import android.util.AttributeSet
+
+class SquareImageView : androidx.appcompat.widget.AppCompatImageView {
+    constructor(context: Context) : super(context)
+    constructor(context: Context, attrs: AttributeSet) : super(context, attrs)
+    constructor(context: Context, attrs: AttributeSet, defStyle: Int) :
+            super(context, attrs, defStyle)
+
+    override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec)
+        setMeasuredDimension(measuredWidth, measuredWidth)
+    }
+}
