dc1ff5afc85b32bf0cf96aaa140dec330daffa49
AnilShatharashi <anil.shatharasi@gmail.com>
2022-04-21 04:24:20 +0200

    Overriding the image dimensions so as to download thumbnail size images

--- app/src/main/java/com/zaloracasestudy/catastrophic/presentation/ui/ImageViewExt.kt
+++ app/src/main/java/com/zaloracasestudy/catastrophic/presentation/ui/ImageViewExt.kt
@@ -2,11 +2,18 @@ package com.zaloracasestudy.catastrophic.presentation.ui
 
 import android.widget.ImageView
 import com.bumptech.glide.Glide
+import com.bumptech.glide.load.engine.DiskCacheStrategy
 import com.bumptech.glide.load.resource.drawable.DrawableTransitionOptions
-
+import com.bumptech.glide.request.RequestOptions
 
 fun ImageView.loadFromUrl(url: String) =
     Glide.with(this.context.applicationContext)
         .load(url)
+        .apply(RequestOptions()
+            .centerCrop()
+            //use dynamic dimensions if we need to support tablets
+            .override(250, 250)
+            .diskCacheStrategy(DiskCacheStrategy.ALL)
+        )
         .transition(DrawableTransitionOptions.withCrossFade())
         .into(this)
